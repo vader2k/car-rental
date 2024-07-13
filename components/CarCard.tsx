76e40cfcbe8f2f78f4e3c3b95c6be6fee3ea1d0d
danie odukwe <kingdanny295@gmail.com>
2024-07-13 09:12:04 +0100

    setting up next to use external image src

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import Image from "next/image"
 import { carProps } from "@/types"
 import Button from "./Button"
-import { calculateCarRent } from "@/utils"
+import { calculateCarRent, generateCarImageUrl } from "@/utils"
 import CardDetails from "./CardDetails"
 
 interface CarCardProps {
@@ -34,7 +34,7 @@ const CarCard = ({ car }: CarCardProps) => {
 
       <div className="relative object-contain w-full h-40 my-3">
         <Image 
-          src="/hero.png" 
+          src={generateCarImageUrl(car)} 
           fill 
           priority 
           alt="car model"
